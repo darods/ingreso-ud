348472654dfb5baf6719848cf1e39655fb70df77
daniel <daniel_arod@protonmail.com>
2022-06-03 13:22:25 -0500

    emojis insted of memes, modeles with 2018-1 data, icfes cant be less than the sum of componentes

--- model-building/ing_catastral_classifier.py
+++ model-building/ing_catastral_classifier.py
@@ -2,16 +2,18 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
 import pickle
 
 
 # read csv data from different years
+df_2018_1 = pd.read_csv('./data/catastral_2018-1.csv')
 df_2022_1 = pd.read_csv('./data/catastral_2022-1.csv')
 
 
 # combine datasets
-frames = [df_2022_1]
+frames = [df_2018_1, df_2022_1]
 data = pd.concat(frames)
 
 # clean unnesary categorie
@@ -22,15 +24,15 @@ data=data[~data['RESULTADO'].isin(['INHABILITADO'])]
 lab_encoder = preprocessing.LabelEncoder()
 
 # define the target
-y = data['RESULTADO']
+#y = data['RESULTADO']
 
 y_num =  lab_encoder.fit_transform(data['RESULTADO'])
 
-
+'''
 mapeo = dict(zip(y, y_num))
 print(mapeo)
 print(y_num, lab_encoder.inverse_transform(y_num))
-
+'''
 #define features
 features = ['ICFES', 'PONDERADO']
 x = data[features].copy()
@@ -39,7 +41,7 @@ x = data[features].copy()
 x_train, x_valid, y_train, y_valid = train_test_split(x, y_num, train_size = 0.8, test_size=0.2, random_state=0)
 
 # define model classifier
-model = RandomForestClassifier()
+model = RandomForestClassifier(n_estimators=500, class_weight={0:1, 1:2})
 model.fit(x_train, y_train)
 y_pred = model.predict(x_valid)
 
@@ -48,8 +50,9 @@ print(model.predict(pd.DataFrame({'ICFES': [343.00, 347.00, 355.00], 'PONDERADO'
 
 # get mean absolute error
 error = mean_absolute_error(y_valid, y_pred)
-
+accuaracy = accuracy_score(y_true=y_valid, y_pred=y_pred)
 print('Error del modelo: ', error)
+print('accuaracy: ', accuaracy)
 
 # save model
 pickle.dump(model, open('ing_catastral_model.pkl', 'wb'))
--- web_app.py
+++ web_app.py
@@ -7,9 +7,9 @@ from PIL import Image
 st.set_page_config(layout="wide")
 
 # get images
-image_happy = Image.open('img/happy_pepe.png')
-image_confused = Image.open('img/confused_pepe.png')
-image_sad = Image.open('img/sad_pepe.png')
+image_happy = Image.open('img/happy_face.png')
+image_confused = Image.open('img/confused_face.png')
+image_sad = Image.open('img/sad_face.png')
 
 st.title('¿Paso a la Distri?')
 st.write('Aplicación **NO OFICIAL** que predice tus chances de entrar a alguna de las carreras de la **Facultad de ingeniería (sede la 40)**')
@@ -21,7 +21,7 @@ col1, col2 = st.columns((4, 2))
 
 
 
-p_icfes = st.sidebar.number_input('ICFES', 0, 500, 350)
+p_icfes = st.sidebar.number_input('ICFES', 0, 500, 360)
 matematicas = st.sidebar.number_input('Matemáticas', 0, 100, 75)
 naturales = st.sidebar.number_input('Ciencias  Naturales', 0, 100, 75)
 lenguaje = st.sidebar.number_input('Lenguaje', 0, 100, 70)
@@ -30,24 +30,30 @@ ingles = st.sidebar.number_input('Ingles', 0, 100, 70)
 
 def input_ingenieria_general():
     # matematicas 35% ciencias 35% lectura 15% sociales 10% ingles 5%
+    icfes_componentes = matematicas+naturales+lenguaje+ciudadanas+ingles 
     ponderado = (matematicas*0.35)+(naturales*0.35)+(lenguaje*0.15)+(ciudadanas*0.1)+(ingles*0.05)
     
     data = {'ICFES': p_icfes,
-            'PONDERADO': ponderado}
+            'PONDERADO': ponderado,
+            'ICFES-COMPONENTES': icfes_componentes}
     features = pd.DataFrame(data, index=[0])
+
+    st.sidebar.write('## ICFES por componentes = ', icfes_componentes)
     st.sidebar.write('## Resultado de ponderado = ', round(ponderado, 3))
 
     return features
 
 def input_catastral():
-
+    icfes_componentes = matematicas+naturales+lenguaje+ciudadanas+ingles
     # matematicas 35% ciencias 35% lectura 15% sociales 10% ingles 5%
     ponderado = (matematicas*0.35)+(naturales*0.30)+(lenguaje*0.15)+(ciudadanas*0.15)+(ingles*0.05)
     
     data = {'ICFES': p_icfes,
-            'PONDERADO': ponderado}
+            'PONDERADO': ponderado,
+            'ICFES-COMPONENTES': icfes_componentes}
     features = pd.DataFrame(data, index=[0])
     
+    st.sidebar.write('## ICFES por componentes = ', icfes_componentes)
     st.sidebar.write('## Resultado de ponderado = ', round(ponderado, 3))
 
     return features
@@ -64,46 +70,59 @@ if carrera == 'Sistemas':
 elif carrera == 'Electrónica':
     df = input_ingenieria_general()
     load_clf = pickle.load(open('model-building/ing_electronica_model.pkl', 'rb'))
+    with col2:
+        st.metric(label= 'tasa de acierto inteligencia artificial', value='81.373 %', delta='-21.078 % de error de predicción')
 
 elif carrera == 'Eléctrica':
     df = input_ingenieria_general()
     load_clf = pickle.load(open('model-building/ing_electrica_model.pkl', 'rb'))
+    with col2:
+        st.metric(label= 'tasa de acierto inteligencia artificial', value='77.778 %', delta='-31.944 % de error de predicción')
 
 elif carrera == 'Industrial':
     df = input_ingenieria_general()
     load_clf = pickle.load(open('model-building/ing_industrial_model.pkl', 'rb'))
+    with col2:
+        st.metric(label= 'tasa de acierto inteligencia artificial', value='89.595 %', delta='-12.717 % de error de predicción')
+
 
 elif carrera == 'Catastral':
     df = input_catastral()
     load_clf = pickle.load(open('model-building/ing_catastral_model.pkl', 'rb'))
+    with col2:
+        st.metric(label= 'tasa de acierto inteligencia artificial', value='74.747 %', delta='-31.313 % de error de predicción')
 
 
 
 if df['ICFES'][0] > 280:
-    # Apply model to make predictions
-    prediction = load_clf.predict(df)
-    prediction_proba = load_clf.predict_proba(df)
+    if df['ICFES'][0]>=df['ICFES-COMPONENTES'][0]:
+        # Apply model to make predictions
+        prediction = load_clf.predict(df[['ICFES', 'PONDERADO']])
+        prediction_proba = load_clf.predict_proba(df[['ICFES', 'PONDERADO']])
 
-    clasificacion = np.array(['ADMITIDO','NO ADMITIDO','OPCIONADO'])
+        clasificacion = np.array(['ADMITIDO','NO ADMITIDO','OPCIONADO'])
 
-    with col1:
-        st.subheader('Resultado de predicción')
-        estado_prediccion = str(clasificacion[prediction][0])
-        st.title(estado_prediccion)
+        with col1:
+            st.subheader('Resultado de predicción')
+            estado_prediccion = str(clasificacion[prediction][0])
+            st.title(estado_prediccion)
 
-        if estado_prediccion == 'ADMITIDO':
-            st.image(image_happy, caption='¡CelebrALO CuRRAMbA!')
-        elif estado_prediccion == 'OPCIONADO':
-            st.image(image_confused, caption='La fé es lo último que se pierde')
-        else:
-            st.image(image_sad, caption='¡No te desanimes! Vuélvelo a intentar')
+            if estado_prediccion == 'ADMITIDO':
+                st.image(image_happy, caption='¡Felicidades!')
+            elif estado_prediccion == 'OPCIONADO':
+                st.image(image_confused, caption='Te darán un cupo si algún admitido no entra')
+            else:
+                st.image(image_sad, caption='¡No te desanimes! Vuélvelo a intentar')
 
 
-    with col2:
-        st.subheader('Probabilidad')
-        st.write('Admitido:', round(prediction_proba[0][0]*100, 3), ' %')
-        st.write('Opcionado:', round(prediction_proba[0][2]*100, 3), ' %')
-        st.write('No admitido:', round(prediction_proba[0][1]*100, 3), ' %')
+        with col2:
+            st.subheader('Probabilidad')
+            st.write('Admitido:', round(prediction_proba[0][0]*100, 3), ' %')
+            st.write('Opcionado:', round(prediction_proba[0][2]*100, 3), ' %')
+            st.write('No admitido:', round(prediction_proba[0][1]*100, 3), ' %')
+    
+    else:
+        st.subheader('El ICFES ingresado no puede ser menor a la suma de sus componentes')
 else:
     st.subheader('Puntaje menor al mínimo requerido (280 puntos) por la facultad de ingeniería')
-    st.image(image_sad, caption='¡No te desanimes! Dios le da las peores batallas a sus mejores guerreros')
+    st.image(image_sad, caption='¡No te desanimes! Vuélvelo a intentar')
